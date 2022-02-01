397d3f034f5372a72ffe116fb956c4e90d793b36
Hammad <muhammadhammadali112233@gmail.com>
2022-02-01 12:13:43 +0500

    Stroing Data online Step#1:object Destructuring Step#2 Validation Step#3 Store data code Step#4 Save Data All process using promisses

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,10 @@
 const express=require('express');
 
+require('../db/conn');
+
+const User = require('../model/userSchema');
+
+
 // is kay andar router ke file ho ge
 const router = express.Router();
 
@@ -8,10 +13,33 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/register',(req,res)=>{
-    console.log(req.body)
-    res.send("This is Register Pge");
+    // console.log(req.body)
+    // res.send("This is Register Pge");
 
-    res.json({message:req-body})
+    // res.json({message:req.body})
+
+    // Object Destructuring
+    const {name,email,phone,work,password,cpassword}=req.body;
+
+    // Here Authentication 
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error:'Please Fille All The field'})
+    }
+    else{
+        User.findOne({email:email}).then((userExit)=>{
+            if(userExit){
+                return res.status(422).json({error:"Email Is Not Matched"})
+            }
+        })
+
+        const user=new User({name,email,phone,work,password,cpassword})
+
+        user.save().then(()=>{
+            console.log("User Data")
+        }).catch(err=>{console.log("Data not Save")})
+
+    }
+
 })
 
 module.exports=router;
