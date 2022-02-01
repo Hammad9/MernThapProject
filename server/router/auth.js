895b69c57c851ab5c766822ac1dceace440da34b
Hammad <muhammadhammadali112233@gmail.com>
2022-02-01 21:57:45 +0500

    In This Project we create the async awiat and try catch Before we use Program using promises

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 
 require('../db/conn');
 
@@ -8,10 +8,13 @@ const User = require('../model/userSchema');
 // is kay andar router ke file ho ge
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send("This is Router Home Page")
 })
 
+// Below Using Promises
+
+/*
 router.post('/register',(req,res)=>{
     // console.log(req.body)
     // res.send("This is Register Pge");
@@ -24,8 +27,11 @@ router.post('/register',(req,res)=>{
     // Here Authentication 
     if(!name || !email || !phone || !work || !password || !cpassword){
         return res.status(422).json({error:'Please Fille All The field'})
+   
+   
     }
-    else{
+    
+        // user Already Existing email to nae addd kar raha
         User.findOne({email:email}).then((userExit)=>{
             if(userExit){
                 return res.status(422).json({error:"Email Is Not Matched"})
@@ -40,6 +46,38 @@ router.post('/register',(req,res)=>{
 
     }
 
-})
+)
+*/
 
-module.exports=router;
+
+// Using Async Await
+router.post('/register', async (req, res) => {
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    // Here Authentication 
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: 'Please Fille All The field' })
+    }
+    try {
+
+        // user Already Existing email to nae addd kar raha
+        const userExit = await User.findOne({ email: email })
+        if (userExit) {
+            return res.status(422).json({ error: "Email Is Not Matched" })
+        }
+
+        const user = new User({ name, email, phone, work, password, cpassword })
+
+        await user.save()
+        res.status(201).json({ message: "User Register Succefully" })
+    }
+
+    catch (err) {
+        console.log(err)
+    }
+
+}
+
+)
+
+module.exports = router;
