cd283a44053f2218cf05a89e10b54378b9f5b84d
Hammad <muhammadhammadali112233@gmail.com>
2022-03-09 12:20:07 +0500

    Here we Design the Lecture no #24 Home page Ui Design

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-        <p className="pt-5">Welcome</p>
-        <h1>We Are Mern Developer</h1>
+    <div style={{textAlign:'center',marginTop:250,}}>
+        <p className="pt-5" style={{fontSize:100,}}>Welcome</p>
+        <h1 style={{marginTop:70, fontSize:30}}>We Are Mern Developer</h1>
     </div>
   )
 }
