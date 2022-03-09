28cfc60a3f2070aa70de7770da599bc237e307f1
Hammad <muhammadhammadali112233@gmail.com>
2022-03-09 09:53:00 +0500

    We Add Css in Project and also add the css in bootstrap file

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import {Link} from 'react-router-dom'
+import Logo from '../Images/batman.png'
 
 function Navbar() {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">
+            <img style={{width:60, height:40} } src={Logo}  />
+          </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
+            <ul style={{marginLeft:1000,} }  className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
