b3e906169ec4ac8be8949e2b4c48eed0bbfaaf59
Hammad <muhammadhammadali112233@gmail.com>
2022-03-15 23:26:31 +0500

    this is 26 Lecutre and we Create the Error page designing

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import About from './Components/About'
 import Contact from './Components/Contact'
 import Signup from './Components/Signup'
 import Login from './Components/Login'
+import Eror from './Components/Eror'
+
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 
 
@@ -20,6 +22,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/eror" element={<Eror />} />
 
         </Routes>
       </Router>
---
+++ client/src/Components/Eror.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Eror = () => {
+  return (
+    <div className="container">
+      <h1 style={{fontWeight:'bold',color:'black',fontSize:40, textAlign:'center',marginTop:230}}>We are Sorry, Page Not Found</h1>
+      <Button className="btn btn-primary">Primary</Button>
+      </div>
+  )
+}
+
+export default Eror
