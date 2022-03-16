cff02d3d66f8ab8dcbb9173b73068c272b59081d
Hammad <muhammadhammadali112233@gmail.com>
2022-03-16 16:39:43 +0500

    Here we Create the use state and store data for complex input  when we write the any text in the input field its store the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import About from './Components/About'
--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../src/CSSFile/SignUp.css'
 
 function Signup() {
+
+  const [inputData, setInputData] = useState({
+    username:"",
+    email:"",
+    number:"",
+    proffesion:"",
+    password:"",
+    password2:"",
+  })
+  let name, value;
+  const changeData=(event)=>{
+     name= event.target.name;
+     value=event.target.value;
+
+    //  store the data of input data and name ke jo value mean jo ham type kar rahay hai second wo 
+    // name ke jo value hai dynamic data, when we write any thing in the name we get its value
+    setInputData({...inputData,[name]:value})
+  }
+
   return (
     <div>
       <section>
@@ -10,12 +29,12 @@ function Signup() {
             <div class="left">
               <h1>Sign up</h1>
 
-              <input type="text" name="username" placeholder="Your Name" />
-              <input type="text" name="email" placeholder="Your E-mail" />
-              <input type="text" name="number" placeholder="Mobile Number" />
-              <input type="text" name="proffesion" placeholder="Your Proffession" />
-              <input type="password" name="password" placeholder="Password" />
-              <input type="password" name="password2" placeholder="Confirm Password" />
+              <input type="text" name="username" placeholder="Your Name" value={inputData.username} onChange={changeData}/>
+              <input type="text" name="email" placeholder="Your E-mail" value={inputData.email} onChange={changeData}/>
+              <input type="text" name="number" placeholder="Mobile Number" value={inputData.number} onChange={changeData}/>
+              <input type="text" name="proffesion" placeholder="Your Proffession" value={inputData.proffesion} onChange={changeData}/>
+              <input type="password" name="password" placeholder="Password" value={inputData.password} onChange={changeData}/>
+              <input type="password" name="password2" placeholder="Confirm Password" value={inputData.password2} onChange={changeData}/>
 
               <input type="submit" name="signup_submit" value="Register" />
             </div>
